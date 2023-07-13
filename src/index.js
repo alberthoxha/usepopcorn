b8a55871b4ffb83591e1071661b776c0d40880e5
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-07-13 05:56:07 +0200

    delete boiler plate

--- src/index.js
+++ src/index.js
@@ -1,40 +1,13 @@
-import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 
 import StarRating from "./StarRating"
 import App from "./App"
 
-// function Test() {
-//   const [movieRating, setMovieRating] = useState(0)
-
-//   return (
-//     <div>
-//       <StarRati ng
-//         color="orangered"
-//         maxRating={5}
-//         onSetRating={setMovieRating}
-//       />
-//       <p>This movie was rated {movieRating} stars</p>
-//     </div>
-//   )
-// }
-
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating
-      maxRaring={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating
-      size={24}
-      color="blue"
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    /> */}
-    {/* <Test /> */}
-    {/* <App /> */}
+  
   </React.StrictMode>
 )
